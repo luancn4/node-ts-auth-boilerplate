1d3963d91714ae0c46fcc4ed6c08782835eb4d9e
Luan <luancnunes04@gmail.com>
2025-09-01 19:19:56 -0300

    changed config property

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(
   process.env.DB_NAME || "node-auth-boilerplate",
   process.env.DB_USER || "root",
-  process.env.DB_PASS || "rootpassword",
+  process.env.DB_PASSWORD || "rootpassword",
   {
     host: process.env.DB_HOST || "db",
     port: Number(process.env.DB_PORT) || 3306,
